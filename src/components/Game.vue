568bd9fdb6c65efd81dcfa3a0ae014283c3662c9
alex <alusov@ozon.ru>
2020-12-26 00:22:42 +0300

    wip

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -4,10 +4,10 @@
     <div class="game-board">
       <div class="circle">
         <ul>
-          <li class="sector red" data-tile="1"></li>
-          <li class="sector blue" data-tile="2"></li>
-          <li class="sector yellow" data-tile="3"></li>
-          <li class="sector green" data-tile="4"></li>
+          <li class="sector red" id="1" @click="listenUserAction"></li>
+          <li class="sector blue" id="2" @click="listenUserAction" ></li>
+          <li class="sector yellow" id="3" @click="listenUserAction" ></li>
+          <li class="sector green" id="4" @click="listenUserAction" ></li>
         </ul>
       </div>
       <div class="option">
@@ -24,6 +24,20 @@
         </div>
       </div>
     </div>
+    <div id="sound">
+      <audio id="audio1">
+        <source src="1.mp3" type="audio/mpeg" />
+      </audio>
+      <audio id="audio2">
+        <source src="2.mp3" type="audio/mpeg" />
+      </audio>
+      <audio id="audio3">
+        <source src="3.mp3" type="audio/mpeg" />
+      </audio>
+      <audio id="audio4">
+        <source src="4.mp3" type="audio/mpeg" />
+      </audio>
+    </div>
   </div>
 </template>
 
@@ -39,11 +53,81 @@ export default {
       round: 0,
       active: true,
       level: 'easy',
+      activateSimonBoard: false
     }
   },
   methods: {
     start() {
+      this.simonSequence = [];
+      this.userSequence = [];
+      this.round = 0;
+      this.active = true;
+      this.newRound();
+    },
 
+    newRound() {
+      this.round++
+      this.simonSequence.push(this.randomNumber());
+      this.userSequence = this.simonSequence.slice(0);
+      this.animateSector(this.simonSequence);
+    },
+
+    randomNumber() {
+      return Math.ceil((Math.random()*4))
+    },
+
+    animateSector(sequence) {
+      let i = 0;
+      let interval = setInterval(() => {
+        this.playSound(sequence[i])
+        this.lightUp(sequence[i])
+        i++;
+        if (i >= sequence.length) {
+          clearInterval(interval)
+          this.activateSimonBoard = true;
+        }
+      }, 1000);
+    },
+
+    lightUp(item) {
+      let sector = document.getElementById(item)
+      sector.classList.add('animate');
+      setTimeout(() => {
+        sector.classList.remove('animate');
+      }, 1000);
+    },
+
+    playSound(item) {
+      let audio = document.getElementById(`audio${item}`)
+      audio.play();
+    },
+
+    listenUserAction(event) {
+      if(this.activateSimonBoard) {
+        event.target.classList.add('animate');
+        this.playSound(event.target.id);
+        setTimeout(()=>{event.target.classList.remove('animate')}, 300);
+        let correctResponse = this.userSequence.shift();
+        let userResponse = event.target.id;
+        this.active = (userResponse == correctResponse);
+        this.checkLose();
+      }
+    },
+
+    checkLose() {
+      if (this.userSequence.length === 0 && this.active) {
+        this.activateSimonBoard = false;
+        this.newRound();
+
+      } else if (!this.active) {
+        this.activateSimonBoard = false;
+        this.endGame();
+      }
+    },
+
+    endGame() {
+      this.round = 0;
+      this.active = false;
     }
   }
 
